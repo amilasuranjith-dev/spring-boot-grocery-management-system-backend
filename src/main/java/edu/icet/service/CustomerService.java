c5d21db25767db4f1382de98054e845e515f4c3b
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-02-19 15:34:32 +0530

    feat : implement customer management functionality

---
+++ src/main/java/edu/icet/service/CustomerService.java
@@ -0,0 +1,13 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.CustomerDto;
+
+import java.util.List;
+
+public interface CustomerService {
+    boolean addCustomer(CustomerDto customerDto);
+    boolean updateCustomer(CustomerDto customerDto);
+    boolean deleteCustomer(String id);
+    CustomerDto searchCustomer(String id);
+    List<CustomerDto> getAll();
+}
---
+++ src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,40 @@
+package edu.icet.service.impl;
+
+import edu.icet.model.dto.CustomerDto;
+import edu.icet.repository.CustomerRepository;
+import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public boolean addCustomer(CustomerDto customerDto) {
+        return customerRepository.addCustomer(customerDto);
+    }
+
+    @Override
+    public boolean updateCustomer(CustomerDto customerDto) {
+        return customerRepository.updateCustomer(customerDto);
+    }
+
+    @Override
+    public boolean deleteCustomer(String id) {
+        return customerRepository.deleteCustomer(id);
+    }
+
+    @Override
+    public CustomerDto searchCustomer(String id) {
+        return customerRepository.searchCustomer(id);
+    }
+
+    @Override
+    public List<CustomerDto> getAll() {
+        return customerRepository.getAll();
+    }
+}
